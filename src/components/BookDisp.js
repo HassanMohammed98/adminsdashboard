d08c667af70cfded697a2db60415a9be624dc938
Abdullah90 <darkside944@gmail.com>
2022-01-30 13:08:01 +0300

    cards & display

--- src/components/BookDisp.js
+++ src/components/BookDisp.js
@@ -4,9 +4,15 @@ import { Link } from "react-router-dom";
 const BookDisp = ({ book }) => {
   return (
     <Link to={`/Books/${book.slug}`}>
-      <div>
+      {/* <div>
         <h1>title: {book.title}</h1>
         <h4>author: {book.author}</h4>
+      </div> */}
+      <div class="card">
+        <div class="personal-intro">
+          <h4>title: {book.title}</h4>
+          <h6>author: {book.author}</h6>
+        </div>
       </div>
     </Link>
   );
--- src/components/MemberDisp.js
+++ src/components/MemberDisp.js
@@ -4,11 +4,20 @@ import { Link } from "react-router-dom";
 const MemberDisp = ({ member }) => {
   return (
     <Link to={`/Members/${member.slug}`}>
-      <div>
+      {/* <div>
         <h1>
           name: {member.firstName} {member.lastName}
         </h1>
         <h4>membership: {member.membership}</h4>
+      </div> */}
+
+      <div class="card front">
+        <div class="personal-intro">
+          <h3>
+            {member.firstName} {member.lastName}
+          </h3>
+          <h6>membership: {member.membership}</h6>
+        </div>
       </div>
     </Link>
   );
